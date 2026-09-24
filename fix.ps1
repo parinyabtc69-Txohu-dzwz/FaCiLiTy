cdbee6ae80c883efa3ec0b45498f4988d58c1a67
Taohx_dz_parinya <parinya.btc69@gmail.com>
2026-09-24 11:05:50 +0700

    chore: cleanup patch files, relocate backups to Archive, and add .gitignore

--- fix.ps1
+++
@@ -1,7 +0,0 @@
-﻿$path = 'Code.gs'
-$text = [System.IO.File]::ReadAllText($path, [System.Text.Encoding]::UTF8)
-$win874 = [System.Text.Encoding]::GetEncoding(874)
-$utf8 = [System.Text.Encoding]::UTF8
-$bytes = $win874.GetBytes($text)
-$restored = $utf8.GetString($bytes)
-[System.IO.File]::WriteAllText($path, $restored, $utf8)
